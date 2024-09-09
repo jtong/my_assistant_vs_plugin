82e04b3a4d4d473d87f79e60a9e8848f684ba4ac
jtong <tj19832@gmail.com>
2024-09-09 19:36:41 +0800

    support task in message handler

--- messageHandler.js
+++ messageHandler.js
@@ -1,32 +1,52 @@
 // messageHandler.js
-
+const { Task } = require('ai-agent-response');
 const logger = require("./logger");
-
 
 class MessageHandler {
     constructor(threadRepository, agentLoader) {
         this.threadRepository = threadRepository;
-        this.agentLoader = agentLoader
+        this.agentLoader = agentLoader;
     }
 
     async handleMessage(thread, responseHandler, host_utils) {
+        const messageTask = new Task({
+            name: 'Process Message',
+            type: Task.TYPE_MESSAGE,
+            message: thread.messages[thread.messages.length - 1].text,
+            meta: {
+                threadId: thread.id,
+                messageId: thread.messages[thread.messages.length - 1].id
+            },
+            host_utils: host_utils
+        });
+
+        this.handleTask(thread, messageTask, responseHandler);
+    }
+
+    async handleTask(thread, task, responseHandler) {
         const agent = this.agentLoader.loadAgent(thread.agent);
-        const initialResponse = await agent.generateReply(thread, host_utils);
+        let initialResponse;
+
+        if (task.isMessageTask()) {
+            initialResponse = await agent.generateReply(thread, task.message);
+        } else {
+            initialResponse = await agent.executeTask(task, thread);
+        }
 
         if (initialResponse.isPlanResponse()) {
             const taskList = initialResponse.getTaskList();
-            let previousTaskMeta = initialResponse.meta || {};  // 使用初始响应的 meta 或空对象
-            for (const task of taskList) {
-                // 如果任务没有 meta，则初始化为空对象
-                task.meta = task.meta || {};
-                task.meta = { ...previousTaskMeta, ...task.meta };
-                task.host_utils = host_utils;
-                const taskResponse = await agent.executeTask(task, thread);
+            let previousTaskMeta = initialResponse.meta || {};
 
-                // 对每个任务响应异步执行 responseHandler
-                await responseHandler(taskResponse, thread);
+            for (const taskInfo of taskList) {
+                const subTask = new Task({
+                    name: taskInfo.name,
+                    type: taskInfo.type || Task.TYPE_ACTION,
+                    message: taskInfo.message,
+                    meta: { ...previousTaskMeta, ...taskInfo.meta },
+                    host_utils: task.host_utils
+                });
 
-                // 保存当前任务的 response meta 为下一个任务的 previousTaskMeta
+                const taskResponse = await this.handleTask(thread, subTask, responseHandler);
                 previousTaskMeta = taskResponse.meta || {};
             }
         } else {
@@ -47,8 +67,6 @@ class MessageHandler {
         this.threadRepository.addMessage(thread.id, newMessage);
         return this.threadRepository.getThread(thread.id);
     }
-
-
 }
 
 module.exports = MessageHandler;
